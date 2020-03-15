9cec150e5557545efe7e2d0e22a5da54ab830986
raj vaya <rajkumarvaya@gmail.com>
2020-03-15 13:22:00 +0530

    Initial commit with google AUTH

---
+++ lib/loginpage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  _signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final FirebaseUser user =
+          (await _auth.signInWithCredential(credential)).user;
+      print("LOGGDIN AS : " + user.email.toString());
+      return user;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('"Login"'),
+      ),
+      body: Center(
+        child: Text('text'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _signInWithGoogle,
+        label: Text('login'),
+      ),
+    );
+  }
+}
